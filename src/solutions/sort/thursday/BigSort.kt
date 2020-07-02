7eb57caa74ac8f89cf0a4cd39c8c48ff1b1df33b
Max Taraniuk <maksim.taranyk@gmail.com>
2020-07-02 17:11:56 +0300

    add bigO

--- src/solutions/sort/thursday/BigSort.kt
+++ src/solutions/sort/thursday/BigSort.kt
@@ -12,43 +12,53 @@ fun main(args: Array<String>) {
 
 class BigSort {
     var list = arrayListOf<String>()
-    fun initList(inp : String) {
+    fun initList(inp: String) {
         list = readFile(inp)
     }
 
     fun run(inp: String, out: String) {
         initList(inp)
-        runBubbleSort(getListIntNumbers(list[0]),out)
-        runHeapSort(getListIntNumbers(list[1]),out)
-        runInsertionSort(getListIntNumbers(list[2]),out)
-        runQuickSort(getListIntNumbers(list[3]),out)
-        runMergeSort(getListIntNumbers(list[4]),out)
-        runSelectionSort(getListIntNumbers(list[5]),out)
+        runBubbleSort(getListIntNumbers(list[0]), out)
+        runHeapSort(getListIntNumbers(list[1]), out)
+        runInsertionSort(getListIntNumbers(list[2]), out)
+        runQuickSort(getListIntNumbers(list[3]), out)
+        runMergeSort(getListIntNumbers(list[4]), out)
+        runSelectionSort(getListIntNumbers(list[5]), out)
     }
 
+    //O(n^2)
     private fun runBubbleSort(list: ArrayList<Int>, out: String) {
         bubbleSort(list)
-        createFileAndAppendText(out,list,"BubbleSort")
+        createFileAndAppendText(out, list, "BubbleSort")
     }
+
+    //O(nlog n)
     private fun runHeapSort(list: ArrayList<Int>, out: String) {
         heapSort(list)
-        createFileAndAppendText(out,list,"HeapSort")
+        createFileAndAppendText(out, list, "HeapSort")
     }
+
+    //O(n^2)
     private fun runInsertionSort(list: ArrayList<Int>, out: String) {
         insertionSort(list)
-        createFileAndAppendText(out,list,"InsertionSort")
+        createFileAndAppendText(out, list, "InsertionSort")
     }
+
+    //O(n log n)
     private fun runMergeSort(list: ArrayList<Int>, out: String) {
-        mergeSort(list,0,list.size - 1)
-        createFileAndAppendText(out,list,"MergeSort")
+        mergeSort(list, 0, list.size - 1)
+        createFileAndAppendText(out, list, "MergeSort")
     }
+
+    // O(n^2)
     private fun runQuickSort(list: ArrayList<Int>, out: String) {
-        solutions.sort.algoritm.quickSort(list,0,list.size-1)
-        createFileAndAppendText(out,list,"QuickSort")
+        solutions.sort.algoritm.quickSort(list, 0, list.size - 1)
+        createFileAndAppendText(out, list, "QuickSort")
     }
+    // O(n^2)
     private fun runSelectionSort(list: ArrayList<Int>, out: String) {
         selectionSort(list)
-        createFileAndAppendText(out,list,"SelectionSort")
+        createFileAndAppendText(out, list, "SelectionSort")
     }
 
 }
